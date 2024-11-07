b33bcb4c9ef8b32a1b309fd0410cbdc8d9a7e3bc
Virachai Wongsena <wvirachai@gmail.com>
2024-11-07 21:11:39 +0700

    include footer component in App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import React from "react";
 import Navbar from "./components/layout/Navbar";
+import SimpleFooter from "./components/layout/SimpleFooter";
 import { Outlet } from "react-router-dom";
 import { Suspense } from "react";
 
@@ -8,11 +9,12 @@ function App() {
   return (
     <>
       <Navbar />
-      <main id="detail">
+      <main id="detail" className="min-h-svh">
         <Suspense fallback={<div>loading...</div>}>
           <Outlet />
         </Suspense>
       </main>
+      <SimpleFooter />
     </>
   );
 }
---
+++ src/components/layout/SimpleFooter.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+export default function SimpleFooter() {
+  return (
+    <footer>
+      <div className="w-full px-6 py-4 md:flex md:items-center md:justify-between bg-gray-700">
+        <span className="text-sm  text-white sm:text-center">
+          © 2024{" "}
+          <a href="#" className="hover:underline">
+            Dino Verse Team
+          </a>
+          . All Rights Reserved.
+        </span>
+        <ul className="hidden flex-wrap items-center mt-3 text-sm font-medium text-white sm:mt-0 lg:flex">
+          <li>
+            <a href="#" className="hover:underline me-4 md:me-6">
+              About
+            </a>
+          </li>
+          <li>
+            <a href="#" className="hover:underline me-4 md:me-6">
+              Privacy Policy
+            </a>
+          </li>
+          <li>
+            <a href="#" className="hover:underline me-4 md:me-6">
+              Licensing
+            </a>
+          </li>
+          <li>
+            <a href="#" className="hover:underline">
+              Contact
+            </a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
+}
