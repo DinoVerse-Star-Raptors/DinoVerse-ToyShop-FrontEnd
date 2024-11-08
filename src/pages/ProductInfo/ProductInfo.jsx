530f280ef4b292c864912e6521b3b43ea1248a74
Virachai Wongsena <wvirachai@gmail.com>
2024-11-08 09:51:25 +0700

    Create ProductInfo Folder

---
+++ src/pages/ProductInfo/ProductInfo.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+// import uiStyle from "./ProductInfo.module.css";
+import reactLogo from "./assets/logo192.png";
+import { Link } from "react-router-dom";
+
+const ProductInfo = () => {
+  return (
+    <div className="flex flex-col items-center">
+      <div className="flex flex-row justify-center">
+        <Link to="#" target="_blank" rel="noreferrer">
+          <img src={reactLogo} className="logo" alt="React logo" />
+        </Link>
+      </div>
+      <h1>Cart</h1>
+      <div className="card">
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  );
+};
+
+export default ProductInfo;
