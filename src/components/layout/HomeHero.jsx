52348bf6693b7f07084b5614d818731a748e32cd
Silayuth <rockyjimslive@gmail.com>
2024-11-10 21:42:30 +0700

    first commit

---
+++ src/components/layout/HomeHero.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function HomeHero() {
+  return (
+    <section className="flex">
+        <div className="flex flex-col gap-8">
+          <h1>Play, Learn Enjoy & Repeat</h1>
+          <h3>Find your best toys for your children. <br />We deliver best of age appropriate toys to your door</h3>
+          <div className=" bg-blue-500"><a href="#" className=" bg-white">Get Your Toy</a></div>
+        </div>
+        <div></div>
+      </section>
+  )
+}
+
+export default HomeHero
