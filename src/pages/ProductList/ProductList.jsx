17b705352d206024a026aecab8d83a8fb348c05e
Virachai Wongsena <wvirachai@gmail.com>
2024-11-12 09:47:02 +0700

    Edit Product List

---
+++ src/pages/ProductList/ProductList.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+// import PropTypes from "prop-types";
+// import uiStyle from "./ProductList.module.css";
+// import { Link } from "react-router-dom";
+// import data from "../../MockingData";
+
+const ProductList = () => {
+  return <>List</>;
+};
+
+export default ProductList;
