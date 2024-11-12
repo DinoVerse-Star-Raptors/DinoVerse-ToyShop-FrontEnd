8f4a32945957c42c955765acfaa35fa1b47473c8
Silayuth <rockyjimslive@gmail.com>
2024-11-12 09:44:29 +0700

    commit

--- src/components/layout/HomeShopbyage.jsx
+++ src/components/layout/HomeShopbyage.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ShopByAge from './assets/images/Shopbyagepic.jpg'
-import ZeroToSix from './assets/images/0-6M.jpg'
-import SixMonths from './assets/images/6M.jpg'
-import TwelveMonths from './assets/images/12M.jpg'
-import EighteenMonths from './assets/images/18M.jpg'
-import TwoYears from './assets/images/2yrs.jpg'
-import FiveYears from './assets/images/5yrs.jpg'
+import ZeroToSix from './assets/images/0-6M-circle.png'
+import SixMonths from './assets/images/6M-circle.png'
+import TwelveMonths from './assets/images/12M-circle.png'
+import EighteenMonths from './assets/images/18M-circle.png'
+import TwoYears from './assets/images/2yrs-circle.png'
+import FiveYears from './assets/images/5yrs-circle.png'
 
 function HomeShopbyage() {
   return (
@@ -14,28 +14,28 @@ function HomeShopbyage() {
             <img src={ShopByAge} alt="Kids Image" className="rounded-md shadow-lg"/>
         </div>
         <h2 className="text-4xl font-bold  text-center">Shop by Age</h2>
-        <div className="grid grid-cols-1 sm:grid-cols-6 gap-4 m-6">
-            <div className="flex flex-col justify-center items-center gap-2">
-              <img src={ZeroToSix} alt="0-6m" className='rounded-full '/>
+        <div className="grid grid-cols-1 sm:grid-cols-6 gap-4 m-5">
+            <div className="flex flex-col justify-center items-center">
+              <img src={ZeroToSix} alt="0-6m" className=''/>
               <h3>0-6M</h3>
             </div>
-            <div className="flex flex-col justify-center items-center gap-2">
-              <img src={SixMonths} alt="6m" className='rounded-full' />
+            <div className="flex flex-col justify-center items-center ">
+              <img src={SixMonths} alt="6m" className='' />
               <h3>6M+</h3>
             </div>
-            <div className="flex flex-col justify-center items-center gap-2">
-              <img src={TwelveMonths} alt="12m" className='rounded-full' />
+            <div className="flex flex-col justify-center items-center ">
+              <img src={TwelveMonths} alt="12m" className='' />
               <h3>12M+</h3>
             </div>
-            <div className="flex flex-col justify-center items-center gap-2">
-              <img src={EighteenMonths} alt="18M" className='rounded-full' />
+            <div className="flex flex-col justify-center items-center ">
+              <img src={EighteenMonths} alt="18M" className='' />
               <h3>18M+</h3>
             </div>
-            <div className="flex flex-col justify-center items-center gap-2">
+            <div className="flex flex-col justify-center items-center ">
               <img src={TwoYears} alt="2Y" className=' rounded-full' />
               <h3>2Yrs+</h3>
             </div>
-            <div className="flex flex-col justify-center items-center gap-2">
+            <div className="flex flex-col justify-center items-center ">
               <img src={FiveYears} alt="5Y" className='rounded-full'/>
               <h3>5Yrs+</h3>
             </div>
