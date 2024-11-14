4bc3a77ac9d6026344e4d64769ba25e1cbf81c7c
makerporch <makerporch@MacBook-Air-khxng-makerporch.local>
2024-11-14 10:45:37 +0700

    allproduct

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import Navbar from "./components/layout/Navbar";
 import SimpleFooter from "./components/layout/SimpleFooter";
 import { Outlet } from "react-router-dom";
 import { Suspense } from "react";
+import Fillter from "./components/product/Fillter";
+import { Products } from "./components/product/Products";
+
+
 
 function App() {
   return (
--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -2,24 +2,15 @@ import React from "react";
 // import uiStyle from "./Shop.module.css";
 import reactLogo from "./assets/logo192.png";
 import { Link } from "react-router-dom";
+import Fillter from "../../components/product/Fillter";
+import { Products } from "../../components/product/Products";
+import { Buttons } from "@testing-library/user-event/dist/cjs/system/pointer/buttons.js";
 
-const Shop = () => {
+export const Shop = () => {
   return (
-    <div className="flex flex-col items-center">
-      <div className="flex flex-row justify-center">
-        <Link to="#" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo" alt="React logo" />
-        </Link>
-      </div>
-      <h1>Shop</h1>
-      <div className="card">
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className=" flex">
+      <Fillter />
+      <Products />
     </div>
   );
 };
