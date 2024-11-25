a26bcd19b21259e9b96ab2ccee03f803c6a19ba1
Virachai Wongsena <wvirachai@gmail.com>
2024-11-25 17:40:50 +0700

    disable pattern in email

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -355,7 +355,7 @@ function Register() {
                       placeholder="you@example.com"
                       value={formData.email}
                       onChange={handleChange}
-                      pattern="/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/"
+                      // pattern="/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/"
                       className="mt-1 block w-full rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm"
                     />
                   </div>
