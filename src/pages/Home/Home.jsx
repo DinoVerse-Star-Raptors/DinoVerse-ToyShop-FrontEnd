d9c91c3bfefa5d8ebb70a6a812471cb129249526
Virachai Wongsena <wvirachai@gmail.com>
2024-11-29 18:49:22 +0700

    edit home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ProductGrid from "./ProductGrid"; // Import ProductGrid component
 import uiStyle from "./Home.module.css"; // Import CSS Module for Home styles
+import HomeHero from "../../components/layout/HomeHero";
 
 const Home = () => {
   return (
     <div className={uiStyle.home}>
+      <section>
+        <HomeHero />
+      </section>
       <section className="bg-gray-50 px-4 py-8">
         {/* Title for ProductGrid */}
         <h2 className="mb-8 text-center text-4xl font-bold text-gray-800">
